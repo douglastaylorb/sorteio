6d2088cf0c17f6fa51d5787ab3911de26cee094c
Douglas Taylor <78963489+douglastaylorb@users.noreply.github.com>
2024-04-05 15:38:28 -0300

    Adiciona opção Megasena

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
-
-        
-function sortear(){
+function sortearLoto(){
     let sorteio;
     let loto = new Array();
     let texto = 'Este são seus números da sorte:';
     let button = 'Sortear novamente';
     let i=0;
+    
     while (i < 15) {
 
         sorteio = Math.ceil(Math.random()*25);
@@ -15,7 +14,7 @@ function sortear(){
         }
     }
 
-    let background = document.getElementById("resultado--area");
+    let background = document.querySelector(".resultado--area");
     
     if(background.style.padding == '20px') {
         
@@ -23,14 +22,39 @@ function sortear(){
         background.style.padding = "20px";
     } 
 
+    let resultado = loto.join(' - ');
+    document.querySelector(".textoresultado").innerHTML = texto;
+    document.querySelector(".resultado").innerHTML=resultado;
+    document.querySelector(".button").textContent=button;
 
+}
 
-
-
-    let loto2 = loto.join(' - ');
-    document.getElementById("textoresultado").innerHTML = texto;
-    document.getElementById("resultado").innerHTML=loto2;
-    document.getElementById("button").innerHTML=button;
-
+function sortearMega(){
+    let sorteio;
+    let loto = new Array();
+    let texto = 'Este são seus números da sorte:';
+    let button = 'Sortear novamente';
+    let i=0; 
+    while (i < 6) {
+    
+        sorteio = Math.ceil(Math.random()*60);
+        if(loto.indexOf(sorteio) < 0){
+            loto.push(sorteio);
+            i++;
+        }
     }
- 
+    
+     let background = document.querySelector(".resultado--area");
+        
+     if(background.style.padding == '20px') {
+          
+     } else {
+            background.style.padding = "20px";
+        } 
+    
+    let resultado = loto.join(' - ');
+    document.querySelector(".textoresultado").innerHTML = texto;
+    document.querySelector(".resultado").innerHTML=resultado;
+    document.querySelector(".button").textContent=button;
+    
+}
